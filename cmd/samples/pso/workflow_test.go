5c40d2f5fcfaeeb30c89fa9551144d2639a3895f
Marco Fiocco <marco.fiocco@autodesk.com>
2019-05-10 17:00:23 +0100

    Cleanup test

--- cmd/samples/pso/workflow_test.go
+++ cmd/samples/pso/workflow_test.go
@@ -4,33 +4,25 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/require"
+
 	"go.uber.org/cadence/activity"
 	"go.uber.org/cadence/encoded"
 	"go.uber.org/cadence/testsuite"
 )
 
-type UnitTestSuite struct {
-	suite.Suite
-	testsuite.WorkflowTestSuite
-}
-
-func TestUnitTestSuite(t *testing.T) {
-	suite.Run(t, new(UnitTestSuite))
-}
-
-func (s *UnitTestSuite) Test_SampleFileProcessingWorkflow() {
-	expectedCall := []string{
-		"evaluateFitnessActivity",
-	}
+func Test_Workflow(t *testing.T) {
+	testSuite := &testsuite.WorkflowTestSuite{}
+	env := testSuite.NewTestWorkflowEnvironment()
 
 	var activityCalled []string
-	env := s.NewTestWorkflowEnvironment()
+
+	//env.OnActivity(evaluateFitnessActivity, mock.Anything, "sphere", []float64{1.0, 2.0, 3.0}).Return(14.0, nil)
 	env.SetOnActivityStartedListener(func(activityInfo *activity.Info, ctx context.Context, args encoded.Values) {
 		activityType := activityInfo.ActivityType.Name
 		activityCalled = append(activityCalled, activityType)
 		switch activityType {
-		case expectedCall[0]:
+		case "evaluateFitnessActivity":
 			// var input string
 			// s.NoError(args.Get(&input))
 			// s.Equal(fileID, input)
@@ -38,9 +30,11 @@ func (s *UnitTestSuite) Test_SampleFileProcessingWorkflow() {
 			panic("unexpected activity call")
 		}
 	})
+
 	env.ExecuteWorkflow(PSOWorkflow, "sphere")
 
-	s.True(env.IsWorkflowCompleted())
-	s.NoError(env.GetWorkflowError())
-	//s.Equal(expectedCall, activityCalled) //activityCalled is a vector with many activities called
+	//env.AssertExpectations(t) // assert any OnActivity and Return
+	require.True(t, env.IsWorkflowCompleted())
+	require.NoError(t, env.GetWorkflowError())
+	//require.Equal(t, "evaluateFitnessActivity", activityCalled) //activityCalled is a vector with many activities called
 }
