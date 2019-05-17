44c8d79e1bef118536eb33f2488994232868caca
Quanzheng Long <longer@uber.com>
2019-05-17 14:44:24 -0700

    Fix names

--- cmd/samples/recipes/helloworld/replay_test.go
+++ cmd/samples/recipes/helloworld/replay_test.go
@@ -12,27 +12,27 @@ import (
 	"go.uber.org/cadence/.gen/go/cadence/workflowservicetest"
 )
 
-type internalWorkerTestSuite struct {
+type replayTestSuite struct {
 	suite.Suite
 	mockCtrl *gomock.Controller
 	service  *workflowservicetest.MockClient
 }
 
-func TestInternalWorkferTestSuite(t *testing.T) {
-	s := new(internalWorkerTestSuite)
+func TestReplayTestSuite(t *testing.T) {
+	s := new(replayTestSuite)
 	suite.Run(t, s)
 }
 
-func (s *internalWorkerTestSuite) SetupTest() {
+func (s *replayTestSuite) SetupTest() {
 	s.mockCtrl = gomock.NewController(s.T())
 	s.service = workflowservicetest.NewMockClient(s.mockCtrl)
 }
 
-func (s *internalWorkerTestSuite) TearDownTest() {
+func (s *replayTestSuite) TearDownTest() {
 	s.mockCtrl.Finish() // assert mock’s expectations
 }
 
-func (s *internalWorkerTestSuite) TestReplayWorkflowHistoryFromFile() {
+func (s *replayTestSuite) TestReplayWorkflowHistoryFromFile() {
 	logger, _ := zap.NewDevelopment()
 	err := worker.ReplayWorkflowHistoryFromJSONFile(logger, "helloworld.json")
 	require.NoError(s.T(), err)
