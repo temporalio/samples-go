3ce3f7500d63db85458399c241c6601c44b4c136
Quanzheng Long <longer@uber.com>
2019-07-01 09:46:04 -0700

    stash

--- cmd/samples/recipes/cancelactivity/workflow.go
+++ cmd/samples/recipes/cancelactivity/workflow.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"time"
-
-	"strconv"
 
 	"go.uber.org/cadence"
 	"go.uber.org/cadence/activity"
@@ -14,7 +11,7 @@ import (
 )
 
 /**
- * This is the hello world workflow sample.
+ * This is the cancel activity workflow sample.
  */
 
 // ApplicationName is the task list for this sample
@@ -24,7 +21,8 @@ const ApplicationName = "cancelGroup"
 // and activity function handlers.
 func init() {
 	workflow.Register(Workflow)
-	activity.Register(cancelActivity)
+	activity.Register(activityToBeCacneled)
+	activity.Register(cleanupActivity)
 }
 
 // Workflow workflow decider
@@ -38,29 +36,22 @@ func Workflow(ctx workflow.Context) error {
 	logger := workflow.GetLogger(ctx)
 	logger.Info("cancel workflow started")
 
-	err := workflow.ExecuteActivity(ctx, cancelActivity).Get(ctx, nil)
+	err := workflow.ExecuteActivity(ctx, activityToBeCacneled).Get(ctx, nil)
 	if err != nil {
 		if cadence.IsCanceledError(err) {
 			// here, we need to get a new ctx to perform cleanup
 			newCtx, _ := workflow.NewDisconnectedContext(ctx)
-			// MyCancelActivity will delete that record from DB
-			err = workflow.ExecuteActivity(newCtx, cancelActivity, "cleanUp", 1).Get(ctx, nil)
+			err = workflow.ExecuteActivity(newCtx, cleanupActivity).Get(ctx, nil)
 			if err != nil {
-				logger.Error("Cleanup activity failed, not expected", zap.Error(err))
+				logger.Error("Cleanup activity failed", zap.Error(err))
 				return err
 			}
 		} else {
-			logger.Error("Activity failed.", zap.Error(err))
+			logger.Error("Activity activityToBeCacneled failed.", zap.Error(err))
 			return err
 		}
-	}
+	} else {
 
-	//try to use old ctx, it won't be executed. Instead, it will return an canceled error
-	err = workflow.ExecuteActivity(ctx, cancelActivity, "tryOldContext", 1).Get(ctx, nil)
-	if err != nil {
-		logger.Error("Activity failed, this is expected", zap.Error(err))
-		// if returning this err here, the workflow will finished as Canceled.
-		//return err
 	}
 
 	logger.Info("Workflow completed.")
@@ -68,16 +59,26 @@ func Workflow(ctx workflow.Context) error {
 	return nil
 }
 
-func cancelActivity(ctx context.Context, name string, waitSecs int) error {
+func activityToBeCacneled(ctx context.Context) error {
 	//"operationToCancel---cancel workflow after start. Using command 'cadence --do samples-domain wf cancel -w <WorkflowID>' ", 240
-	fmt.Println("first line in cancelActivity " + name)
 	logger := activity.GetLogger(ctx)
-	logger.Info("activity started")
-	select {
-	case <-time.After(time.Duration(waitSecs) * time.Second):
-		logger.Info("wake up after sleep for " + strconv.Itoa(waitSecs) + " seconds")
-	case <-ctx.Done():
-		logger.Info("context is cancelled")
+	logger.Info("activity started, you can use ./cancelactivity -m cancel <WorkflowID> or CLI: 'cadence --do samples-domain wf cancel -w <WorkflowID>' to cancel")
+	for {
+		select {
+		case <-time.After(1 * time.Second):
+			logger.Info("heartbeating...")
+			activity.RecordHeartbeat(ctx, "")
+		case <-ctx.Done():
+			logger.Info("context is cancelled")
+			return nil
+		}
 	}
+
+	return nil
+}
+
+func cleanupActivity(ctx context.Context) error {
+	logger := activity.GetLogger(ctx)
+	logger.Info("cleanupActivity started")
 	return nil
 }
