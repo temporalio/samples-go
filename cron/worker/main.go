d26f7ab7bdc35577cb4701e9538912225dc0a7a5
Alex Shtin <alex@shtin.com>
2020-03-18 16:03:37 -0700

    Move cron to the root.

---
+++ cron/worker/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+
+	"go.temporal.io/temporal/client"
+	"go.temporal.io/temporal/worker"
+	"go.uber.org/zap"
+
+	"github.com/temporalio/temporal-go-samples/cmd/samples/cron"
+)
+
+var (
+	logger *zap.Logger
+)
+
+func main() {
+	var err error
+	logger, err = zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+
+	c, w := startWorker()
+	// The workers are supposed to be long running process that should not exit.
+	// Use channel to wait for Ctrl+C.
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+	// Stop worker, close connection, clean up resources.
+	w.Stop()
+	_ = c.CloseConnection()
+}
+
+// This needs to be done as part of a bootstrap step when the process starts.
+// The workers are supposed to be long running.
+func startWorker() (client.Client, worker.Worker) {
+	c, err := client.NewClient(client.Options{
+		HostPort: client.DefaultHostPort,
+	})
+	if err != nil {
+		logger.Fatal("Unable to create client", zap.Error(err))
+	}
+
+	workerOptions := worker.Options{
+		Logger: logger,
+	}
+
+	w := worker.New(c, "cron-task-list", workerOptions)
+
+	w.RegisterWorkflow(cron.SampleCronWorkflow)
+	w.RegisterActivity(cron.SampleCronActivity)
+
+	err = w.Start()
+	if err != nil {
+		logger.Fatal("Unable to start worker", zap.Error(err))
+	}
+
+	return c, w
+}
