b6dbae68c575e7512f2ab4ab1ba65c646f730e2a
sw-yx <shawnthe1@gmail.com>
2021-05-04 05:56:45 +0800

    add first fixture

---
+++ temporal-fixtures/openNclosed/worker/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+
+	"github.com/temporalio/samples-go/temporal-fixtures/openNclosed"
+)
+
+func main() {
+	// The client and worker are heavyweight objects that should be created once per process.
+	c, err := client.NewClient(client.Options{
+		HostPort: client.DefaultHostPort,
+	})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, "retry-activity", worker.Options{})
+
+	w.RegisterWorkflow(openNclosed.Workflow)
+	w.RegisterActivity(openNclosed.Activity)
+
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+}
