924740c540a0350c84282477d102bb1d10330fd3
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2021-05-06 02:23:46 -0700

    Create large payloads fixture (#94)

---
+++ temporal-fixtures/largepayload/worker/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+
+	"github.com/temporalio/samples-go/temporal-fixtures/largepayload"
+)
+
+func main() {
+	// The client and worker are heavyweight objects that should be created once per process.
+	c, err := client.NewClient(client.Options{
+		HostPort: client.DefaultHostPort,
+	})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, "largepayload", worker.Options{})
+
+	w.RegisterWorkflow(largepayload.LargePayloadWorkflow)
+	w.RegisterActivity(&largepayload.Activities{})
+
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+}
