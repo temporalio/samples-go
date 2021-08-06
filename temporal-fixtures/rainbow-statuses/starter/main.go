3bf2df02c8cc0227d6e3ce608c04bc1af821951a
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2021-08-06 12:31:44 -0700

    Update temporal-fixtures/rainbow-statuses/starter/main.go

--- temporal-fixtures/rainbow-statuses/starter/main.go
+++ temporal-fixtures/rainbow-statuses/starter/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"time"
 
-	// "fmt"
 	"log"
 
 	"github.com/pborman/uuid"
@@ -26,10 +25,9 @@ func main() {
 	}
 	defer c.Close()
 
-	uuidvar := uuid.New()
 	i := 1
 	for i <= NumberOfSets {
-		id := uuidvar[:6]
+		id := uuid.New()[:6]
 		i++
 
 		statuses := []enums.WorkflowExecutionStatus{
@@ -43,14 +41,13 @@ func main() {
 		}
 
 		for _, s := range statuses {
-
 			workflowOptions := client.StartWorkflowOptions{
 				ID:        id + "_" + s.String(),
 				TaskQueue: "rainbow-statuses",
 			}
 
 			if s == enums.WORKFLOW_EXECUTION_STATUS_TIMED_OUT {
-				workflowOptions.WorkflowExecutionTimeout = 1 * time.Second
+				workflowOptions.WorkflowExecutionTimeout = time.Second
 			}
 
 			w, err := c.ExecuteWorkflow(context.Background(), workflowOptions,
@@ -62,13 +59,14 @@ func main() {
 
 			switch s {
 			case enums.WORKFLOW_EXECUTION_STATUS_CANCELED:
-				c.CancelWorkflow(context.Background(), w.GetID(), w.GetRunID())
+				if err = c.CancelWorkflow(context.Background(), w.GetID(), w.GetRunID()); err != nil {
+					log.Fatalln("Unable to Cancel workflow", err)
+				}
 			case enums.WORKFLOW_EXECUTION_STATUS_TERMINATED:
-				c.TerminateWorkflow(context.Background(), w.GetID(), w.GetRunID(), "sample termination")
-
+				if err = c.TerminateWorkflow(context.Background(), w.GetID(), w.GetRunID(), "sample termination"); err != nil {
+					log.Fatalln("Unable to Terminate workflow", err)
+				}
 			}
-
 		}
-
 	}
 }
