214a566da46482b294b25245afac13ac9c10bda2
swyx <shawnthe1@gmail.com>
2021-09-30 00:54:58 +0800

    Update activities.go

--- greetings/activities.go
+++ greetings/activities.go
@@ -2,10 +2,18 @@ package greetings
 
 import "fmt"
 
+// @@@SNIPSTART samples-go-dependency-sharing-activities
 type Activities struct {
 	Name     string
 	Greeting string
 }
+
+// SayGreeting Activity.
+func (a *Activities) SayGreeting(greeting string, name string) (string, error) {
+	result := fmt.Sprintf("Greeting: %s %s!\n", greeting, name)
+	return result, nil
+}
+// @@@SNIPEND
 
 // GetName Activity.
 func (a *Activities) GetName() (string, error) {
@@ -17,8 +25,3 @@ func (a *Activities) GetGreeting() (string, error) {
 	return a.Greeting, nil
 }
 
-// SayGreeting Activity.
-func (a *Activities) SayGreeting(greeting string, name string) (string, error) {
-	result := fmt.Sprintf("Greeting: %s %s!\n", greeting, name)
-	return result, nil
-}
