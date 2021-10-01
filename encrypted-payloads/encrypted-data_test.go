19454ed3044620a61802fe4a1c058d715f4ae267
Rob Holland <rob@temporal.io>
2021-10-01 09:28:15 +0100

    Update tests.

--- encrypted-payloads/encrypted-data_test.go
+++ encrypted-payloads/encrypted-data_test.go
@@ -32,16 +32,16 @@ func Test_DataConverter(t *testing.T) {
 		converter.GetDefaultDataConverter(),
 	)
 
-	defaultPayload, err := defaultDc.ToPayload("Testing")
+	defaultPayloads, err := defaultDc.ToPayloads("Testing")
 	require.NoError(t, err)
 
-	encryptedPayload, err := cryptDc.ToPayload("Testing")
+	encryptedPayloads, err := cryptDc.ToPayloads("Testing")
 	require.NoError(t, err)
 
-	require.NotEqual(t, defaultPayload.GetData(), encryptedPayload.GetData())
+	require.NotEqual(t, defaultPayloads.Payloads[0].GetData(), encryptedPayloads.Payloads[0].GetData())
 
 	var result string
-	err = cryptDc.FromPayload(encryptedPayload, &result)
+	err = cryptDc.FromPayloads(encryptedPayloads, &result)
 	require.NoError(t, err)
 
 	require.Equal(t, "Testing", result)
