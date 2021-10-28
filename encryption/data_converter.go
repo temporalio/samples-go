1d2cab913a9571dfdde2be0b9a95347a016e64f7
Rob Holland <rob@temporal.io>
2021-10-28 17:43:42 +0100

    Typo.

--- encryption/data_converter.go
+++ encryption/data_converter.go
@@ -37,7 +37,7 @@ type Encoder struct {
 }
 
 // TODO: Implement workflow.ContextAware in EncodingDataConverter
-// Note that you only need to implment this function if you need to vary the encryption KeyID per workflow.
+// Note that you only need to implement this function if you need to vary the encryption KeyID per workflow.
 func (dc *DataConverter) WithWorkflowContext(ctx workflow.Context) converter.DataConverter {
 	if val, ok := ctx.Value(PropagateKey).(CryptContext); ok {
 		parent := dc.parent
@@ -55,7 +55,7 @@ func (dc *DataConverter) WithWorkflowContext(ctx workflow.Context) converter.Dat
 }
 
 // TODO: Implement workflow.ContextAware in EncodingDataConverter
-// Note that you only need to implment this function if you need to vary the encryption KeyID per workflow.
+// Note that you only need to implement this function if you need to vary the encryption KeyID per workflow.
 func (dc *DataConverter) WithContext(ctx context.Context) converter.DataConverter {
 	if val, ok := ctx.Value(PropagateKey).(CryptContext); ok {
 		parent := dc.parent
