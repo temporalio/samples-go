3b70b3a8bc317b0627073e73a00a9b50e7bb5566
Rob Holland <rob@temporal.io>
2022-03-26 13:27:12 +0000

    Include a side effect and a local activity.
    This is to show a more varied history, helpful when testing the codec support.

--- codec-server/workflow.go
+++ codec-server/workflow.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/google/uuid"
 	"go.temporal.io/sdk/activity"
 	"go.temporal.io/sdk/workflow"
 )
@@ -15,13 +16,32 @@ func Workflow(ctx workflow.Context, input string) (string, error) {
 	ao := workflow.ActivityOptions{
 		StartToCloseTimeout: 10 * time.Second,
 	}
+	lao := workflow.LocalActivityOptions{
+		StartToCloseTimeout: 10 * time.Second,
+	}
 	ctx = workflow.WithActivityOptions(ctx, ao)
+	ctx = workflow.WithLocalActivityOptions(ctx, lao)
 
 	logger := workflow.GetLogger(ctx)
 	logger.Info("Codec Server workflow started", "input", input)
 
 	var result string
-	err := workflow.ExecuteActivity(ctx, Activity, input).Get(ctx, &result)
+
+	err := workflow.SideEffect(ctx, func(ctx workflow.Context) interface{} {
+		return uuid.New()
+	}).Get(&result)
+	if err != nil {
+		logger.Error("SideEffect failed.", "Error", err)
+		return "", err
+	}
+
+	err = workflow.ExecuteLocalActivity(ctx, Activity, input).Get(ctx, &result)
+	if err != nil {
+		logger.Error("Local Activity failed.", "Error", err)
+		return "", err
+	}
+
+	err = workflow.ExecuteActivity(ctx, Activity, input).Get(ctx, &result)
 	if err != nil {
 		logger.Error("Activity failed.", "Error", err)
 		return "", err
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
 	github.com/golang/mock v1.6.0
 	github.com/golang/snappy v0.0.4
+	github.com/google/uuid v1.3.0
 	github.com/hashicorp/go-plugin v1.4.3
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/pborman/uuid v1.2.1
