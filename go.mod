7edcf1d54032f39638cba8ce460b081aa8c6db36
Chad Retz <chad@temporal.io>
2022-11-15 13:45:50 -0600

    Update Tally Prometheus sample (#191)

--- go.mod
+++ go.mod
@@ -20,8 +20,8 @@ require (
 	go.temporal.io/api v1.13.0
 	go.temporal.io/sdk v1.18.1
 	go.temporal.io/sdk/contrib/opentracing v0.1.0
-	go.temporal.io/sdk/contrib/tally v0.1.0
-	go.temporal.io/sdk/contrib/tools/workflowcheck v0.0.0-20220331154559-fd0d1eb548eb
+	go.temporal.io/sdk/contrib/tally v0.2.0
+	go.temporal.io/sdk/contrib/tools/workflowcheck v0.0.0-20220909152124-c1ce1d25238b
 	go.temporal.io/server v1.15.2
 	go.uber.org/multierr v1.7.0
 	go.uber.org/zap v1.20.0
--- metrics/worker/main.go
+++ metrics/worker/main.go
@@ -53,35 +53,12 @@ func newPrometheusScope(c prometheus.Configuration) tally.Scope {
 	scopeOpts := tally.ScopeOptions{
 		CachedReporter:  reporter,
 		Separator:       prometheus.DefaultSeparator,
-		SanitizeOptions: &sanitizeOptions,
+		SanitizeOptions: &sdktally.PrometheusSanitizeOptions,
 		Prefix:          "temporal_samples",
 	}
 	scope, _ := tally.NewRootScope(scopeOpts, time.Second)
+	scope = sdktally.NewPrometheusNamingScope(scope)
 
 	log.Println("prometheus metrics scope created")
 	return scope
 }
-
-// tally sanitizer options that satisfy Prometheus restrictions.
-// This will rename metrics at the tally emission level, so metrics name we
-// use maybe different from what gets emitted. In the current implementation
-// it will replace - and . with _
-var (
-	safeCharacters = []rune{'_'}
-
-	sanitizeOptions = tally.SanitizeOptions{
-		NameCharacters: tally.ValidCharacters{
-			Ranges:     tally.AlphanumericRange,
-			Characters: safeCharacters,
-		},
-		KeyCharacters: tally.ValidCharacters{
-			Ranges:     tally.AlphanumericRange,
-			Characters: safeCharacters,
-		},
-		ValueCharacters: tally.ValidCharacters{
-			Ranges:     tally.AlphanumericRange,
-			Characters: safeCharacters,
-		},
-		ReplacementCharacter: tally.DefaultReplacementCharacter,
-	}
-)
