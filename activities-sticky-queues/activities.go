96c0cc0aa3a7678d12ed240b3e692ea3837c870e
Quinn Klassen <klassenq@gmail.com>
2023-01-30 07:42:50 -0800

    Respond to pr comments

--- activities-sticky-queues/activities.go
+++ activities-sticky-queues/activities.go
@@ -1,6 +1,7 @@
 package activities_sticky_queues
 
 import (
+	"bytes"
 	"context"
 	"crypto/sha256"
 	"io"
@@ -11,44 +12,32 @@ import (
 )
 
 // DownloadFile creates a file on the host with some test data.
-func DownloadFile(ctx context.Context, url, path string) (err error) {
+func DownloadFile(ctx context.Context, url, path string) error {
 	logger := activity.GetLogger(ctx)
 	logger.Info("Downloading file", "url", url, "path", path)
-	// Create the file
-	out, err := os.Create(path)
-	if err != nil {
-		return
-	}
-	defer func() {
-		err = out.Close()
-	}()
 	time.Sleep(3 * time.Second)
-	_, err = out.WriteString("downloaded body")
-	return
+	return os.WriteFile(url, []byte("Hello, Gophers!"), 0666)
 }
 
 // ProcessFile is a stub function to processes a file.
-func ProcessFile(ctx context.Context, path string) (err error) {
+func ProcessFile(ctx context.Context, path string) error {
 	logger := activity.GetLogger(ctx)
-	// Open file
-	f, err := os.Open(path)
+	// Read from file
+	data, err := os.ReadFile(path)
 	if err != nil {
-		return
+		return err
 	}
-	defer func() {
-		err = f.Close()
-	}()
 
 	// Calculate checksum
 	h := sha256.New()
-	if _, err = io.Copy(h, f); err != nil {
-		return
+	if _, err = io.Copy(h, bytes.NewReader(data)); err != nil {
+		return err
 	}
 	checksum := h.Sum(nil)
 
 	logger.Info("Did some work", "path", path, "checksum", checksum)
 	time.Sleep(3 * time.Second)
-	return
+	return nil
 }
 
 // DeleteFile deletes path file on the host
