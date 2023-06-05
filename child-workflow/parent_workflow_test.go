f605379167732ea47bfc0b06b285cff4eb011471
Edmondo Porcu <edmondo.porcu@gmail.com>
2023-06-04 21:12:08 -0700

    Adding example on how to test parent/child workflows in simple case

---
+++ child-workflow/parent_workflow_test.go
@@ -0,0 +1,22 @@
+package child_workflow
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.temporal.io/sdk/testsuite"
+)
+
+func Test_Workflow(t *testing.T) {
+	testSuite := &testsuite.WorkflowTestSuite{}
+	env := testSuite.NewTestWorkflowEnvironment()
+	env.RegisterWorkflow(SampleChildWorkflow)
+
+	env.ExecuteWorkflow(SampleParentWorkflow)
+
+	require.True(t, env.IsWorkflowCompleted())
+	require.NoError(t, env.GetWorkflowError())
+	var result string
+	require.NoError(t, env.GetWorkflowResult(&result))
+	require.Equal(t, "Hello World!", result)
+}
