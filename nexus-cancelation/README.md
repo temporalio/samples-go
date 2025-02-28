7cbb8b9ac096539134547a956f47f1c911a3e712
Roey Berman <roey@temporal.io>
2025-02-27 17:22:14 -0800

    Add nexus-cancelation sample (#393)
    See also https://github.com/temporalio/samples-java/pull/718

---
+++ nexus-cancelation/README.md
@@ -0,0 +1,44 @@
+# Nexus Cancellation
+
+This sample shows how to cancel a Nexus operation from a caller workflow.
+
+From more details on Nexus and how to setup to run this samples please see the [Nexus Sample](../nexus/README.md).
+
+### Running the sample
+
+In separate terminal windows:
+
+### Nexus handler worker
+
+```
+cd handler
+go run ./worker \
+    -target-host localhost:7233 \
+    -namespace my-target-namespace
+```
+
+### Nexus caller worker
+
+```
+cd caller
+go run ./worker \
+    -target-host localhost:7233 \
+    -namespace my-caller-namespace
+```
+
+### Start caller workflow
+
+```
+cd caller
+go run ./starter \
+    -target-host localhost:7233 \
+    -namespace my-caller-namespace
+```
+
+### Output
+
+which should result in:
+```
+2025/02/27 12:57:40 Started workflow WorkflowID nexus_hello_caller_workflow_20240723195740 RunID c9789128-2fcd-4083-829d-95e43279f6d7
+2025/02/27 12:57:40 Workflow result: ¡Hola! Nexus 👋
+```
---
+++ nexus-cancelation/caller/workflows.go
@@ -0,0 +1,84 @@
+package caller
+
+import (
+	"errors"
+
+	"github.com/temporalio/samples-go/nexus/service"
+	"go.temporal.io/sdk/temporal"
+	"go.temporal.io/sdk/workflow"
+)
+
+const (
+	TaskQueue    = "my-caller-workflow-task-queue"
+	endpointName = "my-nexus-endpoint-name"
+)
+
+var languages = []service.Language{
+	service.EN,
+	service.FR,
+	service.DE,
+	service.ES,
+	service.TR,
+}
+
+type result struct {
+	lang   service.Language
+	output service.HelloOutput
+	err    error
+}
+
+func HelloCallerWorkflow(ctx workflow.Context, name string) (string, error) {
+	log := workflow.GetLogger(ctx)
+	c := workflow.NewNexusClient(endpointName, service.HelloServiceName)
+	results := make([]result, len(languages))
+	callCtx, cancel := workflow.WithCancel(ctx)
+	defer cancel()
+
+	// Concurrently execute an operation per language.
+	wg := workflow.NewWaitGroup(ctx)
+	for i, lang := range languages {
+		wg.Add(1)
+		// Use the cancelable callCtx for executing the operation.
+		workflow.Go(callCtx, func(ctx workflow.Context) {
+			defer wg.Done()
+			fut := c.ExecuteOperation(
+				ctx,
+				service.HelloOperationName,
+				service.HelloInput{Name: name, Language: lang},
+				workflow.NexusOperationOptions{})
+			var output service.HelloOutput
+			// This future gets resolved when the operation completes with either success, failure, timeout,
+			// or cancelation.
+			// Only asynchronous operations may receive cancelation as cancelation in Nexus is sent using an
+			// operation token.
+			// The workflow or any other underlying resource backing the operation may choose to ignore the
+			// cancelation request, allowing the operation to end up in any of the terminal states.
+			err := fut.Get(ctx, &output)
+			results[i] = result{
+				lang:   lang,
+				output: output,
+				err:    err,
+			}
+			// The first operation to win the race cancels the others.
+			cancel()
+		})
+	}
+	// Wait for all operations to resolve. Once the workflow completes, the server will stop trying to cancel any
+	// operations that have not yet recieved cancelation, letting them run to completion. It is totally valid to
+	// abandon operations for certain use cases.
+	wg.Wait(ctx)
+
+	var greeting string
+	for _, res := range results {
+		if res.err != nil {
+			// Only return an error if an operation errored out not due to cancelation.
+			if errors.As(res.err, new(*temporal.CanceledError)) {
+				log.Info("operation canceled", "operation", service.HelloOperationName, "lang", res.lang)
+				continue
+			}
+			return "", res.err
+		}
+		greeting = res.output.Message
+	}
+	return greeting, nil
+}
