061fc439588398b5fb44db23bd1f72e6db436eda
Spencer Judge <spencer@temporal.io>
2026-04-01 14:21:06 -0700

    Fix binary build clash

--- lambda-worker/deploy-lambda.sh
+++ lambda-worker/deploy-lambda.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 FUNCTION_NAME="${1:?Usage: deploy-lambda.sh <function-name>}"
 
-GOOS=linux GOARCH=amd64 go build -tags lambda.norpc -o bootstrap main.go
+GOOS=linux GOARCH=amd64 go build -tags lambda.norpc -o bootstrap ./worker
 zip function.zip bootstrap client.pem client.key temporal.toml otel-collector-config.yaml
 aws lambda update-function-code --function-name "$FUNCTION_NAME" --zip-file fileb://function.zip
